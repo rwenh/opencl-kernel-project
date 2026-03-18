4bf70d4170cae3802cca7a35593317cd2a322aec
rwenh <linuxmintoem@gmail.com>
2026-03-18 11:43:51 +0530

    dispatch.hpp

---
+++ include/opencl_wrapper/pipeline.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+//----------Unified Include------------------------------------------------
+#include"platform.hpp"
+#include"context.hpp"
+#include"program.hpp"
+#include"buffer.hpp"
+#include"dispatch.hpp"
